d544aae9b52afbe25ab9b5a2c958454025d118a1
Daniel H <naduran@live.se>
2021-10-19 14:07:05 +0200

    made viewmodel and controlfunction for schema

---
+++ LMS.Core/Models/ViewModels/VeckoSchemaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Core.Models.ViewModels
+{
+    public class VeckoSchemaViewModel
+    {
+        public string WeekDay { get; set; }
+        public string Name { get; set; }
+        public string ActivityTypeName { get; set; }
+        public bool DeadLine { get; set; }
+        //public bool Submitted { get; set; } //To be added if the code for handling handins exists
+    }
+}
--- LMS.Web/Controllers/StudentsController.cs
+++ LMS.Web/Controllers/StudentsController.cs
@@ -13,24 +13,25 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using System.Security.Claims;
 using Lms.Core.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lms.Web.Controllers
 {
     public class StudentsController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly LmsDbContext _context;
 
-
-        public StudentsController(IUnitOfWork unitOfWork)
+        public StudentsController(LmsDbContext context, IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-
-            return View();
-
+            var test = await VeckoSchema();
+            return View(test);
         }
 
         public async Task<IActionResult> CourseDetail()
@@ -49,6 +50,53 @@ namespace Lms.Web.Controllers
                 Documents = course.Documents
             };
             return PartialView("GetCourseDetailsPartial", model);
+        }
+
+        public async Task<IEnumerable<VeckoSchemaViewModel>> VeckoSchema()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var UserLoggedIn = await _unitOfWork.UserRepository.FirstOrDefaultAsync(userId);
+            var courseId = UserLoggedIn.CourseId;
+
+            var modules = await _context.Modules
+                .Include(m => m.Activities).ThenInclude(a=>a.ActivityType)
+                .Where(m => m.CourseId == courseId && m.EndDate > DateTime.Today && m.StartDate <= DateTime.Today.AddDays(7))
+                .OrderBy(m => m.StartDate).ToListAsync();
+
+            List<Activity> activities = new();
+            foreach (var module in modules)
+            {
+                var act = module.Activities
+                    .Where(a => a.EndDate > DateTime.Today && a.StartDate <= DateTime.Today.AddDays(7))
+                    .OrderBy(a => a.StartDate).ToList();
+                activities.AddRange(act);
+            }
+
+            List<VeckoSchemaViewModel> veckoSchema = new();
+
+            for (int i = 0; i < 7; i++)
+            {
+                var date = DateTime.Today.AddDays(i);
+
+                if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) continue;
+
+                foreach (var activity in activities)
+                {
+                    if (activity.StartDate <= date && activity.EndDate >= date)
+                    {
+                        var dagsSchema = new VeckoSchemaViewModel()
+                        {
+                            WeekDay = date.DayOfWeek.ToString(),
+                            Name = activity.Name,
+                            ActivityTypeName = activity.ActivityType.TypeName,
+                            DeadLine = activity.Deadline == date ? true : false 
+                        };
+                        veckoSchema.Add(dagsSchema);
+                    }
+                }
+            }
+
+            return veckoSchema;
         }
     }
 }
